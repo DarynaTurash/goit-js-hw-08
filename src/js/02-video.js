9d1a2d62fa7bfd28e6588764fc469fbb8d8499ee
DarynaTurash <daryna.turash@gmail.com>
2023-05-07 18:48:24 +0300

    task 2

---
+++ src/js/02-video.js
@@ -0,0 +1,29 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+const timeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+
+const onPlay = function(data) {
+    const currentTimeWatchingVideo = data.seconds;
+    localStorage.setItem(LOCAL_STORAGE_KEY, currentTimeWatchingVideo);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(timeFromLocalStorage).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+        break;
+
+        default:
+        break;
+    }
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
